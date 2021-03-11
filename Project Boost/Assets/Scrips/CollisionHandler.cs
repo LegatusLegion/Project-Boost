fdd288827afc77b43af940944e17289ed48a6db5
LegatusLegion <ith3n1nja@gmail.com>
2021-03-11 14:50:18 +0100

    Added Levels

---
+++ Project Boost/Assets/Scrips/CollisionHandler.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Hit Friendly");
+                break;
+
+            case "Fuel":
+                Debug.Log("Hit Fuel");
+                break;
+
+            case "Finish":
+                LoadNextLevel();
+                Debug.Log("finish");
+                break;
+
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    private static void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
